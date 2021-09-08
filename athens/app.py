2de95fbf13c3814b1e30d244ae6003901a211324
Stefan Eng <eng_steff@outlook.com>
2021-09-08 19:23:15 +0200

    Generate secret key if missing

--- athens/app.py
+++ athens/app.py
@@ -1,8 +1,20 @@
 import os
+import secrets
 
 from flask import Flask
+from pathlib import Path
 
 app = Flask(__name__)
+
+PATH_ROOT = Path(__file__).parent.parent
+PATH_DATA = Path(os.getenv('PATH_ATHENS_DATA', PATH_ROOT / 'data'))
+if not PATH_DATA.is_dir():
+    PATH_DATA.mkdir()
+
+PATH_SECRET = PATH_DATA / 'secret.txt'
+if not PATH_SECRET.is_file():
+    PATH_SECRET.write_text(secrets.token_hex())
+app.secret_key = PATH_SECRET.read_text()
 
 
 @app.route('/')
